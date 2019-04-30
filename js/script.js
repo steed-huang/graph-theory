4761cec830ccf9f9684ccc0b8a7d3f91a5b323fc
Steed Huang <steedhuang2002@gmail.com>
2019-04-30 15:15:51 -0400

    added basic random graph generation and drawing (no weights)

--- js/script.js
+++ js/script.js
@@ -23,24 +23,82 @@ class Graph {
     let keys = this.adjList.keys();
     for (let key of keys) {
       let val = this.adjList.get(key);
-      drawCircle(val[0].x, val[0].y);
+      drawNode(key, val[0].x, val[0].y);
+      for (let i = 0; i < val[1].length; i++) {
+        // lines are drawn twice -> improve later
+        let pos = this.adjList.get(val[1][i][0])[0];
+        drawLine(val[0].x, val[0].y, pos.x, pos.y);
+      }
     }
   }
 }
 
-function drawCircle(x, y) {
+function drawText(str, x, y) {
+  c.font = "20px Arial";
+  c.fillStyle = "black";
+  c.textAlign = "center";
+  c.fillText(str, x, y);
+}
+
+function drawNode(str, x, y) {
   c.beginPath();
-  c.arc(x, y, 8, 0, 2 * Math.PI);
+  c.arc(x, y, 15, 0, 2 * Math.PI);
   c.fillStyle = "red";
   c.fill();
   c.lineWidth = 2;
   c.stroke();
+  drawText(str, x, y + 7);
 }
 
-function log() {
+function drawLine(x1, y1, x2, y2) {
+  c.beginPath();
+  c.moveTo(x1, y1);
+  c.lineTo(x2, y2);
+  c.stroke();
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateGraph(num, edges) {
   let graph = new Graph();
-  graph.addVertex("A", 200, 300);
-  graph.addVertex("B", 400, 300);
-  graph.addEdge(["A", "B"], 1);
+  let vertName = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let edgeAcc = 0;
+
+  // first node
+  graph.addVertex("A", getRandom(100, 500), getRandom(100, 500));
+
+  for (let i = 1; i < num; i++) {
+    let x = getRandom(10, 590);
+    let y = getRandom(10, 590);
+    graph.addVertex(vertName.charAt(i), x, y);
+
+    let n = getRandom(0, i - 1);
+    graph.addEdge([vertName.charAt(i), vertName.charAt(n)], 1);
+    edgeAcc++;
+  }
+
+  // there is definitely a better way to do this
+  // this breaks most of the time if edges is larger than num | please fix
+  var n1, n2;
+  if (edgeAcc < edges) {
+    for (let i = 0; i < edges - edgeAcc; i++) {
+      let isNew = false;
+      while (isNew == false) {
+        n1 = getRandom(0, edgeAcc);
+        n2 = getRandom(0, edgeAcc);
+        if (n1 != n2) {
+          for (let z = 0; z < graph.adjList.get(vertName.charAt(n1)).length; z++)
+            if (graph.adjList.get(vertName.charAt(n1))[1][i][0] != vertName.charAt(n2)) {
+              isNew = true;
+            }
+        }
+      }
+      graph.addEdge([vertName.charAt(n1), vertName.charAt(n2)], 1);
+    }
+  }
+
+  console.log(graph.adjList);
   graph.draw();
 }
